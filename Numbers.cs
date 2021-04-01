3099d3274ed38b15fba5f52ba1d96a98a4535e75
AtyaPaso <atyapaso@students.amikom.ac.id>
2021-04-01 21:20:38 +0700

    first commit

---
+++ Numbers.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Numbers
+    {
+        public int findMinimum(int a, int b)
+        {
+            if (a < b)
+            {
+                return a;
+            } else
+            {
+                return b;
+            }
+        }
+        public int findMinimum(int a, int b, float c)
+        {
+            if (a < b)
+            {
+                if (a < c)
+                {
+                    return a;
+                }
+                else
+                {
+                    return (int)c;
+
+                }
+            }
+            else
+            {
+                if (b < c)
+                {
+                    return b;
+                }
+                else
+                {
+                    return (int)c;
+                }
+            }
+        }
+        public int findMaximum(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+        public int findMaximum(int a, int b, float c)
+        {
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    return a;
+                }
+                else
+                {
+                    return (int)c;
+                }
+            }
+            else { 
+                if (b > c)
+                {
+                    return b;
+                }
+                else
+                {
+                    return (int)c;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+
+            Console.WriteLine("Minimum #1: {0}", numbers.findMinimum(3, 1));
+            Console.WriteLine("Minimum #2: {0}", numbers.findMinimum(3, 2, 4));
+            Console.WriteLine();
+            Console.WriteLine("Maximum #1: {0}", numbers.findMaximum(3, 1));
+            Console.WriteLine("Maximum #2: {0}",numbers.findMaximum(3, 2, 4));
+            Console.ReadKey();
+        }
+    }
+}
